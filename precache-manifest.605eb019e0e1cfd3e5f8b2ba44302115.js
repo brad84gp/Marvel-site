7462c94158ed40698cb0e816cbb4184029d13c41
robert.kruger <bradgp84@gmail.com>
2021-11-07 17:51:52 -0600

    Updates

---
+++ precache-manifest.605eb019e0e1cfd3e5f8b2ba44302115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7598e81f455925ba6da31bb4d217b49",
+    "url": "/brad84gp/Marvel-site/index.html"
+  },
+  {
+    "revision": "48f8d26a1637af916ee1",
+    "url": "/brad84gp/Marvel-site/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "a7c3f78131942aac7df4",
+    "url": "/brad84gp/Marvel-site/static/css/main.9a3d9bb2.chunk.css"
+  },
+  {
+    "revision": "48f8d26a1637af916ee1",
+    "url": "/brad84gp/Marvel-site/static/js/2.f4c1e8bb.chunk.js"
+  },
+  {
+    "revision": "54711622920de054c53d39de19424bee",
+    "url": "/brad84gp/Marvel-site/static/js/2.f4c1e8bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7c3f78131942aac7df4",
+    "url": "/brad84gp/Marvel-site/static/js/main.e0d64dcc.chunk.js"
+  },
+  {
+    "revision": "aab48e7416a474a69845",
+    "url": "/brad84gp/Marvel-site/static/js/runtime-main.60acf187.js"
+  }
+]);
